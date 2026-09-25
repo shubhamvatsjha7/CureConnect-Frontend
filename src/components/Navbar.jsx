46b89a98996da172b5a40b72611fc8d9c9216824
Shubham <shubhamvatsjha7@gmail.com>
2026-09-25 23:58:55 +0530

    Edit Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+    <div className="sticky top-0 z-50 bg-white/60 backdrop-blur-lg flex items-center justify-between text-sm py-4 mb-5 border-b border-white/40 shadow-sm">
       <img
         onClick={() => navigate("/")}
         className="w-28 md:w-40 cursor-pointer"
